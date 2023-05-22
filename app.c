16678a89b0ab932e9db9a7405bb9821ca642c92c
PuemMTH <59855164+PuemMTH@users.noreply.github.com>
2023-05-22 11:11:29 +0000

    master

---
+++ app.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!\n");
+
+    int i1;
+    int i2;
+    int i3;
+    int i4;
+    int i5;
+    int i6;
+    int i7;
+    int i8;
+    int i9;
+    int i10;
+
+
+    int i;
+    for (i = 0; i < 10000; i++){
+        printf("Enter %d\n", i);
+    }
+
+    return 0;
+}
